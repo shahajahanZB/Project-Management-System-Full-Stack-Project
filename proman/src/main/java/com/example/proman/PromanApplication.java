27e4cc3dae63c8b51dfe401417f9aeec8562cc1c
ShahajahanZB <shahajahanbekinalkar@gmail.com>
2026-05-01 16:03:26 +0530

    first commit

---
+++ proman/src/main/java/com/example/proman/PromanApplication.java
@@ -0,0 +1,13 @@
+package com.example.proman;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PromanApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PromanApplication.class, args);
+	}
+
+}
---
+++ proman/src/test/java/com/example/proman/PromanApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.proman;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PromanApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
